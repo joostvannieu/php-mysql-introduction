e251ab391a17b4094b6afa31f78c670924dfa8ea
joostvannieu <joost.vannieuwenhuyse@gmail.com>
2019-10-15 10:58:07 +0200

    day 2 first break commit connection.php:  v established connection to db insert.php:  v created form  v put submitted form in db index.php:  TODO: create table with info from db

--- insert.php
+++ insert.php
@@ -1,35 +1,48 @@
 <?php
     require "connection.php";
 
-    var_dump($_POST);
+    //var_dump($_POST);
 
-    /*
-    $stmt = $pdo->prepare("INSERT INTO student     (first_name, 
-                                                            last_name, 
-                                                            username, 
-                                                            linkedin, 
-                                                            github, 
-                                                            email,
-                                                            preferred_language,
-                                                            avatar,
-                                                            video,
-                                                            quote,
-                                                            quote_author)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-    $stmt->execute([$_POST['first-name'],
-                    $_POST['last-name'],
-                    $_POST['username'],
-                    $_POST['linkedin'],
-                    $_POST['github'],
-                    $_POST['email'],
-                    $_POST['pref-lang'],
-                    $_POST['avatar'],
-                    $_POST['video'],
-                    $_POST['quote'],
-                    $_POST['author']
-    ]);
-    $stmt = null;
-    */
+    function addNewStudent(PDO $pdo, $data)
+    {
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = 'INSERT INTO student (first_name, last_name, username, linkedin, github, email, preferred_language, avatar, video, quote, quote_author) VALUES (:first_name, :last_name, :username, :linkedin, :github, :email, :preferred_language, :avatar, :video, :quote, :quote_author)';
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->bindParam(':first_name', $first_name);
+        $stmt->bindParam(':last_name', $last_name);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':linkedin', $linkedin);
+        $stmt->bindParam(':github', $github);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':preferred_language', $preferred_language);
+        $stmt->bindParam(':avatar', $avatar);
+        $stmt->bindParam(':video', $video);
+        $stmt->bindParam(':quote', $quote);
+        $stmt->bindParam(':quote_author', $quote_author);
+
+        $first_name = $data['first_name'];
+        $last_name = $data['last_name'];
+        $username = $data['username'];
+        $linkedin = $data['linkedin'];
+        $github = $data['github'];
+        $email = $data['email'];
+        $preferred_language = $data['preferred_language'];
+        $avatar = $data['avatar'];
+        $video = $data['video'];
+        $quote = $data['quote'];
+        $quote_author = $data['quote_author'];
+
+        $stmt->execute();
+        $stmt = null;
+
+    }
+
+    if (!empty($_POST)){
+        addNewStudent(openConnection(), $_POST);
+        unset($_POST);
+    }
+
 
 ?>
 
@@ -45,10 +58,10 @@
 <body>
 <form action="insert.php" method="post">
     <fieldset>
-        <label for="first-name">First name</label>
-        <input type="text" name="first-name" id="first-name" required><br>
-        <label for="first-name">Last name</label>
-        <input type="text" name="last-name" id="last-name" required><br>
+        <label for="first_name">First name</label>
+        <input type="text" name="first_name" id="first_name" required><br>
+        <label for="last_name">Last name</label>
+        <input type="text" name="last_name" id="last_name" required><br>
         <label for="username">Username</label>
         <input type="text" name="username" id="username" required><br>
         <label for="linkedin">LinkedIn</label>
@@ -57,8 +70,8 @@
         <input type="text" name="github" id="github"><br>
         <label for="email">E-mail</label>
         <input type="email" name="email" id="email" required><br>
-        <label for="pref-lang">Preferred Language</label>
-        <input list="languages" name="pref-lang">
+        <label for="preferred_language">Preferred Language</label>
+        <input list="languages" name="preferred_language">
             <datalist id="languages">
                 <option value="En">
                 <option value="Nl">
@@ -70,10 +83,10 @@
         <input type="url" name="video" id="video"><br>
         <label for="quote">Quote</label>
         <textarea name="quote" id="quote" rows="4" cols="100"></textarea><br>
-        <label for="author">Quote Author</label>
-        <input type="text" name="author" id="author">
+        <label for="quote_author">Quote Author</label>
+        <input type="text" name="quote_author" id="quote_author">
         <br><br>
-        <input type="submit" value="Submit">
+        <input type="submit" value="Save">
     </fieldset>
 </form>
 </body>
